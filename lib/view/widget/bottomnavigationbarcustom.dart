fb3c1c860d4e24c295b410ca5727355a578a0daf
mohAk970 <sssy6669@gmail.com>
2025-05-20 21:05:00 +0300

    تعديل

--- lib/view/widget/bottomnavigationbarcustom.dart
+++ lib/view/widget/bottomnavigationbarcustom.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/cupertino.dart';
 
-class BottomNavigationBarCustom extends StatelessWidget{
-  @override
-  final Widget? icon;
-  final String? label;
-  BottomNavigationBarCustom({ this.icon, this.label});
-  Widget build (BuildContext context){
-    return BottomNavigationBarItem(icon: ,label: "$label");
+class BottomNavigationBarCustom {
+  final Widget icon;
+  final String label;
+
+  BottomNavigationBarCustom({required this.icon, required this.label});
+
+  BottomNavigationBarItem toItem() {
+    return BottomNavigationBarItem(
+      icon: icon,
+      label: label,
+    );
   }
 }
